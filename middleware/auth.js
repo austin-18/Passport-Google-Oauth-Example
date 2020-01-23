cdc0d019ee436406a8d0bfae24150ea26ff91edb
Austin Bracey <austin.bracey18@gmail.com>
2020-01-23 13:34:00 -0600

    updated to have passport isAuthenticated middleware for protected routes

---
+++ middleware/auth.js
@@ -0,0 +1,13 @@
+const passport = require('passport');
+
+// Passport checking if user is already logged in (has token)
+exports.checkGoogleAuth = (req, res, next) => {
+
+    // if user is authenticated in the session, carry on
+    if (req.isAuthenticated()){
+      return next();
+    } else {
+      // if they aren't redirect them to the home page
+      res.redirect('/notAuth');
+    }
+  }
--- routes/passport.js
+++ routes/passport.js
@@ -4,8 +4,11 @@ const router = express.Router();
 const passportGoogle = require('../config/google');
 
 
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+const { checkGoogleAuth } = require('../middleware/auth');
+
+router.get('/notAuth', function(req, res, next) {
+  //res.render('index', { title: 'Express' });
+  res.send('You were not authorized')
 });
 
 router.get('/login', function(req, res, next) {
@@ -15,11 +18,15 @@ router.get('/login', function(req, res, next) {
 router.get('/google', passportGoogle.authenticate('google', {scope : ['profile']}));
 
 router.get('/google/callback',
-  passportGoogle.authenticate('google', { failureRedirect: '/login' }),
+  passportGoogle.authenticate('google', { failureRedirect: '/notAuth' }),
   function(req, res) {
     // Successful authentication
     res.json(req.user);
     res.send(req.user);
   });
+
+router.get('/profile', checkGoogleAuth(), (req, res) => {
+  res.send('you are successfully authenticated')
+})
 
 module.exports = router;
